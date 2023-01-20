071f82369214ddd3aa96c22692867a5047d1f59d
Ishita <ishita11.ishu@gmail.com>
2023-01-20 20:33:35 +0530

    rewrote google sign in code

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'authentication.dart';
-import 'google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(SignInScreen());
@@ -61,12 +60,37 @@ class _SignInScreenState extends State<SignInScreen> {
                     ],
                   ),
                 ),
-                GoogleSignInButton(),
+                ElevatedButton(
+                    onPressed: signIn, child: Text('Sign in with Google'))
               ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+void signIn() {
+  Authentication().signInWithGoogle();
+}
+
+class Authentication {
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
