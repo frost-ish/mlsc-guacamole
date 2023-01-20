bdfe09458f145973d1cb49b8e9a8f4812d5ced79
Ishita <ishita11.ishu@gmail.com>
2023-01-21 01:54:40 +0530

    added user info page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
   runApp(SignInScreen());
@@ -24,58 +25,79 @@ class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 33, 3, 114),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-              bottom: 20.0,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Row(),
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Image.asset(
-                          'assets/image1.png',
-                          height: 160,
-                        ),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 33, 3, 114),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Image.asset(
+                        'assets/image1.png',
+                        height: 160,
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        'MLSC',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
-                        ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'MLSC',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                    onPressed: signIn, child: Text('Sign in with Google'))
-              ],
-            ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Authentication().signInWithGoogle().then(
+                    (value) {
+                      User? user = value.user;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: ((context) {
+                            return SecondPage(user!);
+                          }),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Text('Sign in with Google'),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
-}
-
-void signIn() {
-  Authentication().signInWithGoogle().then((value) {
-    User? user = value.user;
-    print("${user!.providerData[0].displayName} has signed in!");
-  });
 }
 
 class Authentication {
@@ -95,5 +117,71 @@ class Authentication {
 
     // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  User? user;
+  SecondPage(User user) {
+    this.user = user;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 33, 3, 114),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+              right: 16.0,
+              bottom: 20.0,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Flexible(
+                          flex: 1,
+                          child: Image.asset(
+                            'assets/image1.png',
+                            height: 60,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        ('Welcome, ${user!.providerData[0].displayName!}'),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: ElevatedButton(
+                          onPressed: null,
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.blueGrey)),
+                          child: Text(
+                            'Click here to upload your vaccination certificate',
+                            style: TextStyle(color: Colors.white, fontSize: 10),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
